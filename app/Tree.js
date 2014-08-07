33da556338d4ff4136dc325fc90d5c85b0827b31
IsaacFang <isaac.fang@grapecity.com>
2014-08-07 09:08:00 +0800

    On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	new file:   app/Tree.js
    	new file:   app/tree.js

---
+++ app/Tree.js
@@ -0,0 +1,87 @@
+/**
+ * Copyright 2006-2014 GrapeCity inc
+ * Author: isaac.fang@grapecity.com
+ */
+
+(function () {
+
+    function Tree(data) {
+        this.id = data.id;
+    }
+
+    Tree.init = function (data, fn) {
+        var oTree = new Tree(data);
+        oTree.depth = 0;
+
+        (function dfs(oParent, oTree, dTree, index) {
+            oTree.index = index;
+
+            if (oParent) {
+                oTree.parent = oParent;
+                oTree.depth = oParent.depth + 1;
+                oParent.children = oParent.children || [];
+                oParent.children.push(oTree);
+            }
+
+            if (dTree.children) {
+                dTree.children.forEach(function (sTree, index) {
+                    dfs(oTree, new Tree(sTree), sTree, index);
+                });
+            }
+
+            fn && fn(oParent, oTree, dTree, index);
+
+        })(null, oTree, data, 0);
+
+        return oTree;
+    };
+
+    var iterator = Tree.iterator = {
+        dfs: function (tree, prevFn, postFn) {
+            prevFn && prevFn(tree);
+            if (tree.children) {
+                tree.children.forEach(function (child) {
+                    iterator.dfs(child, prevFn, postFn);
+                });
+            }
+            postFn && postFn(tree);
+        },
+        bfs: function (trees, fn) {
+            var children = [];
+            trees.forEach(function (tree) {
+                fn && fn(tree);
+                if (tree.children) {
+                    children = children.concat(tree.children);
+                }
+            });
+            children.length && iterator.bfs(children, fn);
+        }
+    };
+
+    Tree.prototype = {
+        constructor: Tree,
+        dfs: function (prevFn, postFn) {
+            iterator.dfs(this, prevFn, postFn);
+        },
+        bfs: function (fn) {
+            iterator.bfs([this], fn);
+        },
+        createMap: function (fn) {
+            var map = {};
+            this.dfs(function (tree) {
+                if (fn) {
+                    fn(tree, map)
+                } else {
+                    map[tree.id] = tree;
+                }
+            });
+            this.map = map;
+            return map;
+        }
+    };
+
+    window.Tree = Tree;
+
+})();
+
+
